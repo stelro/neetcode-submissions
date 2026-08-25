c21f03146d12fd26984310d7888944893e04b772
Rostislav Orestis Stelmach <stelmach.ro@gmail.com>
2026-08-25 09:28:43 +0100

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        int left = 0;
+        int right = 1;
+
+        while (right < prices.size()) {
+            if (prices[left] > prices[right]) {
+                left = right;
+            } else {
+                profit = std::max(profit, prices[right] - prices[left]);
+            }
+
+            right++;
+        }
+
+        return profit;
+    }
+};
