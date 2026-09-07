df8298bbc96bac31788903f691d77aa503c13f2f
Rostislav Orestis Stelmach <stelmach.ro@gmail.com>
2026-09-07 08:08:30 +0100

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> result (n * 2);
+        for (int i = 0; i < n; i++) {
+            result[i] = result[i + n] = nums[i];
+        }
+        return result;
+    }
+};
